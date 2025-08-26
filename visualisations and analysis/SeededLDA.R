ad5d041fbd3f9697d9a9cd3373771228bc8eaa46
Matthew Grainger <matthew.grainger@nina.no>
2025-08-26 10:10:45 +0200

    reorder libraries

--- visualisations and analysis/SeededLDA.R
+++ visualisations and analysis/SeededLDA.R
@@ -2,6 +2,8 @@
 
 library(seededlda)
 library(synthesisr)
+library(LDAvis)
+library(quanteda)
 
 rayyan <- read_refs("visualisations and analysis/provisional includes 21.12.24.ris")
 
@@ -82,10 +84,6 @@ rayyan$topic <- topic
 
 
 ## Visualise
-library(pacman)
-p_load("seededlda",
-       "quanteda",
-       "LDAvis")
 
 # create a json output
 json <- createJSON(phi = lda_seed$phi, 
